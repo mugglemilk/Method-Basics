a24fc1eb30a3a40a5fce8f46b7b71d382ed51dc3
PUMIWATR_K <120041384+mugglemilk@users.noreply.github.com>
2023-04-27 14:43:51 +0700

    Add files via upload

---
+++ CookiePlan.java
@@ -0,0 +1,11 @@
+
+import java.util.Scanner;
+
+public class CookiePlan {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        double a = 10.2567;
+        System.out.print("aaa %.2f",a);
+    }
+}
