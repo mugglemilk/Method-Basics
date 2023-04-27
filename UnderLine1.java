d768d7f69f26cfcdeeb7bf13243f253e9dec57f6
PUMIWATR_K <120041384+mugglemilk@users.noreply.github.com>
2023-04-27 14:21:40 +0700

    Add files via upload

---
+++ UnderLine1.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+public class UnderLine1 {
+
+    static void Under() {
+        System.out.println("--------------------");
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        for (int i = 0; i < n; ++i) {
+            String str = scan.next();
+            System.out.println(str);
+            Under();
+        }
+    }
+}
